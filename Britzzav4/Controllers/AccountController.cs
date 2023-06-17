ca01181824787503af3483061db4f4c4178ae2aa
Vandinete <vandysouza2@gmail.com>
2023-06-16 23:13:53 -0300

    init commit

--- Britzzav4/Controllers/AccountController.cs
+++ Britzzav4/Controllers/AccountController.cs
@@ -4,16 +4,16 @@ namespace Britzzav4.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class AccountController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<AccountController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public AccountController(ILogger<AccountController> logger)
         {
             _logger = logger;
         }

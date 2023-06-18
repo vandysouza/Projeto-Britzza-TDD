2e118238b3ff320d60e25d6a21e865db96b04a2c
Guilherme Hernandez <familiahernandez0123@gmail.com>
2023-06-18 20:57:47 -0300

    front tela de cadastrar pedidos

---
+++ Britzza - v6/Controllers/PedidosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Britzza___v6.Controllers
+{
+    public class PedidosController : Controller
+    {
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
